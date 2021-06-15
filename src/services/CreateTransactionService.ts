2e0174abe22c9200057d4b1ea98451d06ceb003f
seufernandez <fernandez.g1337@gmail.com>
2021-06-15 00:25:55 -0300

    Complete Application Deploy

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Invalid, that type doesn´t exist');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('Sorry, balance is not enough to face this outcome');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
